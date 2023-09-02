7d2676a3d59571873faa7b14c51d987b396dd857
USMAN KHALID <66887206+USMANKHALID-GH@users.noreply.github.com>
2023-09-02 16:51:41 +0300

    factory constructor

--- OOP/factoryConstuctor.dart
+++ OOP/factoryConstuctor.dart
@@ -1,17 +1,28 @@
 import '../practices/ex5.dart';
 
 void main(List<String> args) {
-  Area(2.3,34).printArea();
+  Area(2.3,-34).printArea();
   
 }
 
 class Area{
   final double length;
-  final width;
+  final double width;
   final double area;
-  const Area(this.length, this.width):  area=width*length;
+  // const Area(this.length, this.width):  area=width*length;
 
   void printArea(){
     print(area);
+  }
+
+  const Area._Area(this.length , this.width): area= length*width;
+
+
+  factory Area(double length, double width){
+    if(length < 0 || width < 0){
+      throw new Exception("width or length cant be zero");
+    }
+    return Area._Area(length, width);
+
   }
 }
